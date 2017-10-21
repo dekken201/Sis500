42e0259bedd79d82880dc6b8cd79e8fcbaa2b42e
Dekken201 <lucksbp@gmail.com>
2017-10-21 15:27:48 -0300

    Integração da biblioteca "pickle" para melhorar a eficiência do programa e algumas melhorias de funcionalidades

--- code/functions.py
+++ code/functions.py
@@ -8,80 +8,97 @@ from io import StringIO
 #OUTROS
 import re
 from fuzzywuzzy import fuzz
-
-#ATRIBUTOS:
+import pickle
 
 #FUNÇÕES:
-def getText(): #Função que extrai o texto do pdf, usando os parâmetros padrão da biblioteca pdfminer
-	rsrcmgr = PDFResourceManager()
-	retstr = StringIO()
-	codec = 'latin1'
-	laparams = LAParams()
-	path = "C:/Users/Luke/Documents/Prog/Python/Sis500/pdf/500pr.pdf"
-	device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-	fp = open(path, 'rb') #Pega o arquivo do pdf no caminho especificado
-	interpreter = PDFPageInterpreter(rsrcmgr, device)
-	password = ""
-	maxpages = 0
-	caching = True
-	pagenos=set()
-	#Roda um loop, usando todos os atributos anteriores, interpretando o pdf por página e realizando a conversão
-	for pagenumber, page in enumerate(PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True)):
-		if pagenumber < 16: #Pula as primeiras 16 páginas, que não possuem nenhuma pergunta/resposta
-			pass
-		else:
-			interpreter.process_page(page)
+def getText(pdfname, filename): #Função que extrai o texto do pdf, usando os parâmetros padrão da biblioteca pdfminer
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'latin1'
+    laparams = LAParams()
+    path = "C:/Users/Luke/Documents/Prog/Python/Sis500/pdf/"+pdfname+".pdf"
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = open(path, 'rb') #Pega o arquivo do pdf no caminho especificado
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+    #Roda um loop, usando todos os atributos anteriores, interpretando o pdf por página e realizando a conversão
+    for pagenumber, page in enumerate(PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True)):
+        if pagenumber < 16: #Pula as primeiras 16 páginas, que não possuem nenhuma pergunta/resposta
+            pass
+        else:
+            if pagenumber >= 100:
+                retstr.write("\n 500pr_pgnumber" + str(pagenumber) +"  ")
+            else:
+                retstr.write("\n 500pr_pgnumber0" + str(pagenumber) +"  ")
+            interpreter.process_page(page)
 
-	text = retstr.getvalue()
 
-	fp.close()
-	device.close()
-	retstr.close()
+    text = retstr.getvalue()
 
-	text_file = open("C:/Users/Luke/Documents/Prog/Python/Sis500/txt/Output.txt", "w")
-	text_file.write(text)
-	text_file.close()
+    fp.close()
+    device.close()
+    retstr.close()
+
+    with open("C:/Users/Luke/Documents/Prog/Python/Sis500/txt/"+filename+".txt", "w") as textFile:
+        textFile.write(text)
 
 
 def getAnswers(filename): #Função que separa as perguntas e respostas do texto minerado do pdf
-	
-	respostas = []
-	perguntas = []
-	file = open("C:/Users/Luke/Documents/Prog/Python/Sis500/txt/"+filename+".txt", "r") 	#Abre o arquivo de texto
-	file = file.read()
-	rgx = re.compile('(?<=\.)[^.]*$') #Compilação do regex a ser utilizado no loop
+    filename = filename
+    filepath = "C:/Users/Luke/Documents/Prog/Python/Sis500/txt/" + filename+ ".txt"
 
-	for i in range(0,507):#até 507; Total de iterações necessárias para pegar todas as perguntas e respostas
-		respostas.append((file.split("?")[i])) #As duas listas recebem a mesma string separadas no "?"
-		perguntas.append(file.split("?")[i])	
+    respostas = []
+    perguntas = []
+    paginas = []
 
-		respostas[i] = re.sub(rgx,'', respostas[i]) #Porém uma pega o regex sem a pergunta(a resposta)
-		perguntas[i] = rgx.findall(perguntas[i]) #e a outra pega o regex que só dá match na pergunta(a pergunta)
+    file = open(filepath,"r")#Abre o arquivo de texto
+    file = file.read()
+    rgx = re.compile('(?<=\.)[^.]*$') #Compilação do regex a ser utilizado no loop
 
-	perguntas[0] = "Qual a origem mais provável do algodoeiro" #Por motivos específicos, o código não consegue pegar a primeira pergunta, então temos que colocá-la manualmente
-	respostas.pop(0) #Pelo mesmo motivo, a primeira resposta é nula, então apagamos ela manualmente
+    for i in range(0,507):#até 507; Total de iterações necessárias para pegar todas as perguntas e respostas
+        splitString = file.split("?")[i]
+        respostas.append(splitString) #As duas listas recebem a mesma string separadas no "?"
+        perguntas.append(splitString)
 
-	return [perguntas, respostas]
+        respostas[i] = re.sub(rgx,'', respostas[i]) #Porém uma pega o regex sem a pergunta(a resposta)
+        perguntas[i] = rgx.findall(perguntas[i]) #e a outra pega o regex que só dá match na pergunta(a pergunta)
 
-def run(lista):
-	perguntas = lista[0]
-	respostas = lista[1]
-	bestMatchIndex = 0
-	bestRatio = 0
+    perguntas[0] = "Qual a origem mais provável do algodoeiro" #Por motivos específicos, o código não consegue pegar a primeira pergunta, então temos que colocá-la manualmente
+    respostas.pop(0) #Pelo mesmo motivo, a primeira resposta é nula, então apagamos ela manualmente
 
-	while True:
-		perguntaUser = input("Digite a sua pergunta: ")
-		for index, pergunta in enumerate(perguntas):
-			ratio = fuzz.ratio(perguntaUser, pergunta)
-			if ratio > bestRatio:
-				bestMatchIndex = index
-				bestRatio = ratio
 
-		print("\nPergunta relacionada: "+ str(perguntas[bestMatchIndex]) +
-		  "\nResposta relacionada: " + respostas[bestMatchIndex])
-		print("\nProximidade da pergunta: " + str(bestRatio))
+    conjunto = [perguntas,respostas]
+    filename = filename.replace("txt","procTxt")
+    filepath = "C:/Users/Luke/Documents/Prog/Python/Sis500/txt/" + filename + ".txt"
+    with open(filepath, 'wb') as fp:
+       pickle.dump(conjunto, fp)
+
+def run(filename):
+    filepath = "C:/Users/Luke/Documents/Prog/Python/Sis500/txt/" + filename + ".txt"
+    with open(filepath, 'rb') as file:
+        lista = pickle.load(file)
+
+    perguntas = lista[0]
+    respostas = lista[1]
+    bestMatchIndex = 0
+    bestRatio = 0
+
+    while True:
+        perguntaUser = input("Digite a sua pergunta: ")
+        for index, pergunta in enumerate(perguntas):
+            ratio = fuzz.ratio(perguntaUser, pergunta)
+            if ratio > bestRatio:
+                bestMatchIndex = index
+                bestRatio = ratio
+
+        print("\nPergunta relacionada: "+ str(perguntas[bestMatchIndex]) +
+          "\nResposta relacionada: " + respostas[bestMatchIndex])
+        print("\nProximidade da pergunta: " + str(bestRatio))
 
 
 def checkRatio(perguntaUser):
-	pass
+    pass
 
--- code/main.py
+++ code/main.py
@@ -1,7 +1,9 @@
 from code.functions import *
 #ALGUMAS PERGUNTAS ESTÃO DESCONFIGURADAS. O INDICE 505 É A PERGUNTA nº 500
 
-#getText()
+#COMPILAÇÃO DO TEXTO E DAS PERGUNTAS:
+#getText("500pr","500pr_txt_algodao")
+#getAnswers("500pr_txt_algodao")
 
-run(getAnswers("Output"))
-
+#RODAR PROGRAMA:
+run("500pr_procTxt_algodao")
