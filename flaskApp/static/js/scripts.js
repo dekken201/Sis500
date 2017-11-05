3e73538a0311d79e7764b1e5cb36b7ef34ec24e6
latocheski <luciano_latocheski@hotmail.com>
2017-11-05 14:18:47 -0300

    Correção caminho elementos gráficos / implementação do sistema de exibição de respostas.

--- flaskApp/static/js/scripts.js
+++ flaskApp/static/js/scripts.js
@@ -1,53 +1,75 @@
+document.getElementById("enviarPerg").addEventListener("click", verifica);
+document.getElementById("assuntos").addEventListener("change", setAssunto);
 
-    $('#txtArea').keypress(function(e) {
-        if (e.keyCode == 13) {
-            gravar();
+var resp = '';
+var top3;
+var assunto = '';
+
+function setAssunto() { //Se alterado o select, atribui a var assunto
+    assunto = $("#assuntos option:selected").val();
+}
+
+$('#txtArea').keypress(function (e) { //Se pressionado enter chama o método
+    if (e.keyCode == 13) {
+        verifica();
+    }
+});
+
+function verifica() { //verifica se esta vazio
+    var msg = $('#txtArea').val();
+    if (assunto != '') {
+        if (msg != '') {
+            enviar(msg);
+        } else {
+            alert("Digite uma pergunta!");
+        }
+    } else {
+        alert("Escolha um assunto primeiro!");
+    }
+}
+
+function enviar(pergunta) {
+    getData(pergunta, assunto);
+    exibirResp();
+}
+
+function getData(pergunta, livro) {
+    $.ajax({
+        url: '/getData',
+        async: false,
+        data: {
+            "pergunta": pergunta,
+            "livro": livro
+        },
+        type: 'POST',
+        success: function (data) {
+            top3 = $.parseJSON(data);
+            //alert(JSON.stringify(resposta["0"]));
+            //alert(JSON.stringify(resposta));
+        },
+        error: function (error) {
+            console.log("Error");
         }
     });
-    var anim = "<div class='animated zoomIn'>";
-    var divChat = $("#cx"); //puxa o div da caixa de msg
-    var resp = '';
-    var top3;
+}
 
-    function gravar() {
-        var msg = $('#txtArea').val(); // puxa o texto do input - acredito que possa enviar para o bot apartir dessa variável
-        if (msg != '') {
-            var divUsuario = anim + "<div class='chat usuario'><br><div class='foto'><img src='{{ url_for('static', filename = 'img/usr.png') }}'></div><br><div class='mensagem'>" + msg + "</div><br></div></div>"
-            divChat.append(divUsuario); //coloca a mensagem na div da caixa
-            $('#txtArea').val(''); // limpa o input
-            $('#cx')[0].scrollTop = $('#cx')[0].scrollHeight; // rola a barra pra baixo
+function exibirResp() {
+    var template = [
+        '<div class="cont">',
+            '<div class="cmsg" id="cx">',
+                '<div class="tit">Pergunta similar:</div>',
+                '<div class="resp">{{pergunta}}</div>',
+                '<div class="cxresp">',
+                    '<div class="tit">Resposta Similar:</div>',
+                    '{{resposta}}',
+                    '<div class="pag">{{pagina}}</div>',
+                '</div>',
+            '</div>',
+        '</div>'
+    ].join("\n");
 
-            resposta(msg); // chama a função da resposta do bot e passa por parâmetro a msg (pergunta)
-
-        }
-        document.getElementById('txtArea').focus(); // joga o cursor para o input novamente se o usuário clicar no botão enviar
+    for (i = 0; i < 3; i++) {
+        var html = Mustache.render(template, top3[i]);
+        $("#cxresp").append(html);
     }
-
-    function resposta(pergunta) {
-        getData(pergunta,"algodao")
-        alert(JSON.stringify(top3))
-
-        var divRobo = anim + "<div class='chat robo'><br><div class='foto'><img src='{{ url_for('static', filename = 'img/logo.png') }}'></div><br><div class='mensagem'>" + lista + "</div><br></div></div>" //concatena
-        divChat.append(divRobo); //coloca dentro da caixa de msgs
-        $('#cx')[0].scrollTop = $('#cx')[0].scrollHeight; //rola pra baixo
-
-    }
-
-     function getData(pergunta,livro){
-        $.ajax({
-            url: '/getData',
-            async:false,
-            data: {
-                "pergunta" : pergunta, "livro" : livro
-            },
-            type: 'POST',
-            success: function(data) {
-                top3 = $.parseJSON(data);
-                //alert(JSON.stringify(resposta["0"]));
-                //alert(JSON.stringify(resposta));
-            },
-            error: function(error) {
-                console.log("Error");
-            }
-        });
-    }
+}
